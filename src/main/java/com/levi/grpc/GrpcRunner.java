cbc8c5deb6af7ec89d9923dd8b607b3bd1610732
levi.yoon <levi.yoon@kakaocorp.com>
2020-05-04 00:31:49 +0900

    feat: improvement grpc server start logic

--- src/main/java/com/levi/grpc/GrpcRunner.java
+++ src/main/java/com/levi/grpc/GrpcRunner.java
@@ -2,10 +2,16 @@ package com.levi.grpc;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @Component
 public class GrpcRunner implements ApplicationRunner {
 
@@ -16,6 +22,21 @@ public class GrpcRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        start();
+    }
+
+    private void start() throws IOException, InterruptedException {
         SERVER.start();
+        log.info("GrpcRunner#start - listen port = {}", PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.error("GrpcRunner#stop - Stopping gRPC server");
+            try {
+                SERVER.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.error("GrpcRunner#stop - gRPC server was stopped");
+        }));
     }
 }
--- src/main/java/com/levi/grpc/SampleServiceImpl.java
+++ src/main/java/com/levi/grpc/SampleServiceImpl.java
@@ -1,8 +1,6 @@
 package com.levi.grpc;
 
-import com.levi.yoon.proto.SampleRequest;
-import com.levi.yoon.proto.SampleResponse;
-import com.levi.yoon.proto.SampleServiceGrpc;
+import com.levi.yoon.proto.*;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +12,13 @@ public class SampleServiceImpl extends SampleServiceGrpc.SampleServiceImplBase {
     @Override
     public void sampleCall(SampleRequest request, StreamObserver<SampleResponse> responseObserver) {
         log.info("SampleServiceImpl#sampleCall - {}, {}", request.getUserId(), request.getMessage());
+        ExampleRequest exampleRequest = ExampleRequest.newBuilder()
+                .putRequests("a", "a")
+                .putRequests("b", "b")
+                .build();
+        exampleRequest.getRequestsMap();
+        exampleRequest.getRequestsOrDefault("a", "defaultValue");
+        exampleRequest.getRequestsOrThrow("a");
         SampleResponse sampleResponse = SampleResponse.newBuilder()
                 .setMessage("grpc service response")
                 .build();
